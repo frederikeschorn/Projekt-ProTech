53323a639373ba9ca07574a209bf9466c03ecfb0
lillikaussler <lillikaussler@gmail.com>
2025-12-19 12:33:23 +0100

    Exception

--- src/main/java/ProjektProTech/Workout.java
+++ src/main/java/ProjektProTech/Workout.java
@@ -8,7 +8,6 @@ import java.awt.event.ActionListener;
 public class Workout extends JFrame{
     private JPanel mainPanel;
     private JLabel datum_label;
-    private JTextField datum_textField1;
     private JLabel gewicht_label;
     private JTextField gewicht_textField1;
     private JLabel sportart_label;
@@ -53,52 +52,67 @@ public class Workout extends JFrame{
     }
 
     public void kalorienBerechnen(){
-        double gewicht = Double.parseDouble(String.valueOf(gewicht_textField1.getText()));
+        String gewichtText = gewicht_textField1.getText().trim(); //Trim= Leerzeichen werden am Anfang und Ende entfernt
+        String dauerText   = dauer_textField1.getText().trim();
 
-        int dauerMin = Integer.parseInt(String.valueOf(dauerMin_textField1.getText()));
-
-       sportart_comboBox1.getSelectedItem();
-       String sportart = sportart_comboBox1.getSelectedItem().toString(); //DropDown wird jedes Mal neu gelesen
-
-       double kalorien;
-
-        if ("Fußball".equals(sportart)){
-            kalorien = 7.0 * gewicht * (dauerMin/60.0); //kalorien = MET * Körpergewicht * Dauer in Stunden
-
-        }else if ("Gehen".equals(sportart)){
-            kalorien = 3.5 * gewicht * (dauerMin/60.0);
-
-        }else if ("Krafttraining".equals(sportart)){
-            kalorien = 8.0 * gewicht * (dauerMin/60.0);
-
-        }else if ("Laufen".equals(sportart)){
-            kalorien = 10.0 * gewicht * (dauerMin/60.0);
-
-        }else if ("Radfahren".equals(sportart)){
-            kalorien = 8.0 * gewicht * (dauerMin/60.0);
-
-        }else if ("Schwimmen".equals(sportart)){
-            kalorien = 5.8 * gewicht * (dauerMin/60.0);
-
-        }else if ("Skifahren".equals(sportart)){
-            kalorien = 5.0 * gewicht * (dauerMin/60.0);
-
-        }else if ("Tennis".equals(sportart)){
-            kalorien = 7.0 * gewicht * (dauerMin/60.0);
-
-        }else if ("Wandern".equals(sportart)){
-            kalorien = 6.0 * gewicht * (dauerMin/60.0);
-
-        }else{
-            kalorien = 2.5 * gewicht * (dauerMin/60.0);
-
+        if (dauerText.contains(",")) {
+            ausgabeKalorien_label.setText("");//alter Wert wird gelöscht und es steht nichts mehr in dem Feld
+            JOptionPane.showMessageDialog(null, "Bitte bei Dauer in Minuten nur ganze Zahlen(ohne Komma) eingeben.", "Eingabefehler", JOptionPane.ERROR_MESSAGE);
+            return;
         }
-        ausgabeKalorien_label.setText(String.format("%.2f",kalorien)); //Ausgabe und runden auf zwei Nachkommastellen
+
+        try {
+            double gewicht = Double.parseDouble(gewichtText);//double, weil nicht nur ganze Zahlen
+            int dauerMin = Integer.parseInt(dauerText); //int, weil nur ganze Zahlen
+
+            sportart_comboBox1.getSelectedItem();
+            String sportart = sportart_comboBox1.getSelectedItem().toString(); //DropDown wird jedes Mal neu gelesen
+
+            double kalorien;
+
+            if ("Fußball".equals(sportart)) {
+                kalorien = 7.0 * gewicht * (dauerMin / 60.0); //kalorien = MET * Körpergewicht * Dauer in Stunden
+
+            } else if ("Gehen".equals(sportart)) {
+                kalorien = 3.5 * gewicht * (dauerMin / 60.0);
+
+            } else if ("Krafttraining".equals(sportart)) {
+                kalorien = 8.0 * gewicht * (dauerMin / 60.0);
+
+            } else if ("Laufen".equals(sportart)) {
+                kalorien = 10.0 * gewicht * (dauerMin / 60.0);
+
+            } else if ("Radfahren".equals(sportart)) {
+                kalorien = 8.0 * gewicht * (dauerMin / 60.0);
+
+            } else if ("Schwimmen".equals(sportart)) {
+                kalorien = 5.8 * gewicht * (dauerMin / 60.0);
+
+            } else if ("Skifahren".equals(sportart)) {
+                kalorien = 5.0 * gewicht * (dauerMin / 60.0);
+
+            } else if ("Tennis".equals(sportart)) {
+                kalorien = 7.0 * gewicht * (dauerMin / 60.0);
+
+            } else if ("Wandern".equals(sportart)) {
+                kalorien = 6.0 * gewicht * (dauerMin / 60.0);
+
+            } else {
+                kalorien = 2.5 * gewicht * (dauerMin / 60.0);
+
+            }
+            ausgabeKalorien_label.setText(String.format("%.2f", kalorien)); //Ausgabe und runden auf zwei Nachkommastellen
+
+        }catch(NumberFormatException g){
+            ausgabeKalorien_label.setText("");
+            JOptionPane.showMessageDialog(null, "Bitte beim Gewicht nur Zahlen eingeben.", "Eingabefehler", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
 
     }
 
     public void inListeSpeichern(){
-        String datum = datum_textField1.getText();
+        String datum = tage_comboBox1.getToolTipText() + monat_comboBox2.getToolTipText() + jahr_comboBox3.getToolTipText();
         String sportart = sportart_comboBox1.getSelectedItem().toString();
         String dauer = dauerMin_textField1.getText();
         String kalorien = ausgabeKalorien_label.getText();
@@ -108,7 +122,6 @@ public class Workout extends JFrame{
     }
 
     public void clearTextfeld(){
-        datum_textField1.setText("");
         dauerMin_textField1.setText("");
     }
 
