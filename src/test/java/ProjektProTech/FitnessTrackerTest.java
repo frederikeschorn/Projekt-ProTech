bed40bee0da6f8a6242df4f55bb146578f2045c1
frederikeschorn <frederike.schorn@student.hnu.de>
2026-01-14 11:01:13 +0100

    Projekt Session 10

--- src/test/java/ProjektProTech/FitnessTrackerTest.java
+++ src/test/java/ProjektProTech/FitnessTrackerTest.java
@@ -7,9 +7,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class FitnessTrackerTest {
 
     @Test
-    void kalorienSumme() {
+    void kalorienBerechnenLaufen() {
         FitnessTracker ft = new FitnessTracker();
-        double v1 = ft.kalorienSumme();
-        assertEquals(1910.0, v1);
+
+        //Eingabefelder setzten
+        ft.gewicht_textField1.setText("70");
+        ft.dauerMin_textField1.setText("60");
+        ft.sportart_comboBox1.setSelectedItem("Laufen");
+
+        //Methode ausführen
+        ft.kalorienBerechnen();;
+
+        //Ergebnis auslesen
+        String ergebnis = ft.ausgabeKalorien_label.getText();
+        double kalorien = Double.parseDouble(ergebnis.replace(",", "."));
+
+        //Erwartetes Ergebnis prüfen
+        assertEquals(700.0, kalorien);
     }
 }
